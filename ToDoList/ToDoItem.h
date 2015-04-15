028a5bf93c5c5837e68379591e2b7bd17a5cd517
Keith Ballinger <keith@standardtreasury.com>
2015-04-15 15:49:54 -0700

    header text change rename

--- ToDoList/ToDoItem.h
+++ ToDoList/ToDoItem.h
@@ -3,7 +3,7 @@
 //  ToDoList
 //
 //  Created by Keith Ballinger on 4/15/15.
-//  Copyright (c) 2015 Keith Ballinger. All rights reserved.
+//  Copyright (c) 2015 Xamarin, inc. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- ToDoList/ToDoList TableViewController.h
+++ ToDoList/ToDoList TableViewController.h
@@ -3,7 +3,7 @@
 //  ToDoList
 //
 //  Created by Keith Ballinger on 4/14/15.
-//  Copyright (c) 2015 Keith Ballinger. All rights reserved.
+//  Copyright (c) 2015 Xamarin, inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
